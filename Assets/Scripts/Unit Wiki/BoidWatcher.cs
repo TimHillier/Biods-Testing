9c920feabaf562387564c4e4583f238613e82cbf
mildlywarm <Tim.r.hillier@gmail.com>
2017-09-13 14:27:02 -0700

    idk
    whats happening?

---
+++ Assets/Scripts/Unit Wiki/BoidWatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidWatcher : MonoBehaviour {
+
+    public Transform boidController;
+    private void LateUpdate()
+    {
+        if(boidController)
+        {
+            Vector3 watchPoint = boidController.GetComponent<BoidController>().flockCenter;
+            transform.LookAt(watchPoint + boidController.transform.position);
+        }
+    }
+}
